dc875ffbacdd39e254e126d5b76aa7585d276969
Eddy2506 <eddy.gutierrez.a@tecsup.edu.pe>
2024-06-04 00:05:16 -0500

    eddy

--- nutri/nutriVite/src/components/EjercicioCard.jsx
+++ nutri/nutriVite/src/components/EjercicioCard.jsx
@@ -1,4 +1,3 @@
-// src/components/EjercicioCard.jsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -7,13 +6,14 @@ export function EjercicioCard({ ejercicio }) {
 
     return (
         <div className=' mt-6 bg-white p-4 rounded-lg shadow-md hover:shadow-lg border border-gray-200'
-         onClick={() => navigate(`/ejercicio/${ejercicio.id}`)}>
+             onClick={() => navigate(`/ejercicio/${ejercicio.id}`)}>
             <div className="grid grid-cols-2 gap-4">
                 <p className="text-sm text-gray-600"><strong>Nombre de Ejercicio:</strong> {ejercicio.nombre_ejercicio}</p>
                 <p className="text-sm text-gray-600"><strong>Descripcion:</strong> {ejercicio.descripcion_ejercicio}</p>
                 <p className="text-sm text-gray-600"><strong>Nivel de dificultad:</strong> {ejercicio.nivel_dificultad}</p>
                 <p className="text-sm text-gray-600"><strong>Tipo de IMC:</strong> {ejercicio.tipo_imc}</p>
-                <img src={ejercicio.images.url} alt="Ejercicio"/>
+                {ejercicio.images && <img src={ejercicio.images} alt="Ejercicio"/>}
+
             </div>
         </div>
     );
