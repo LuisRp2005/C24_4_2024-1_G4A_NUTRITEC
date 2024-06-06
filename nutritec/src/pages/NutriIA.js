2cf6cdc71f2adebe05c062dc5c965c4731381ddb
Eddy2506 <eddy.gutierrez.a@tecsup.edu.pe>
2024-06-06 16:11:53 -0500

    eddy

--- nutritec/src/pages/NutriIA.js
+++ nutritec/src/pages/NutriIA.js
@@ -21,34 +21,43 @@ function NutriIA() {
     return (
         <div className='nutri-ia-container'>
             <NavBar />
-            <div className='App'>
-                <div className='sideBar'>
-                    <div className='upperSide'>
-                        <div className='upperSideTop'><img src='/img_nutricion.png' alt='Logo' className='logo'/><span className='brand'> NutriIA</span></div>
-                        <button className='midBtn'><img src='/suma.png' className='addBtn'/>New Chat</button>
-                        <div className='upperSideBottom'>
-                            <button className='query'><img className='' alt='Query'/>What is programming</button>
-                            <button className='query'><img className='' alt='Query'/>What is programming</button>
-                        </div>
+            <div className='chat-container'>
+                <div className='sidebar'>
+                    <div className='sidebar-header'>
+                        <span className='brand'>NutriIA</span>
+                        <img src='/img_nutricion.png' alt='Logo' className='logo'/>
                     </div>
-                    <div className='lowerSide'>
-                        <div className='listItems'><img src='/home.png' alt='Home' className='listItemsImg'/>Home</div>
-                        <div className='listItems'><img src='/guardar.png' alt='Saved' className='listItemsImg'/>Saved</div>
-                        <div className='listItems'><img src='/rocket.png' alt='Upgrade' className='listItemsImg'/>Upgrade to Pro</div>
+                    <button className='new-chat-btn'><img src='/suma.png' className='addBtn'/>New Chat</button>
+                    <div className='query-list'>
+                        <button className='query'><img className='query-icon' alt='Query'/>What is programming</button>
+                        <button className='query'><img className='query-icon' alt='Query'/>What is programming</button>
+                    </div>
+                    <div className='sidebar-footer'>
+                        <div className='sidebar-item'>
+                            <img src='/home.png' alt='Home' className='sidebar-icon'/>
+                            <span>Home</span>
+                        </div>
+                        <div className='sidebar-item'>
+                            <img src='/guardar.png' alt='Saved' className='sidebar-icon'/>
+                            <span>Saved</span>
+                        </div>
+                        <div className='sidebar-item'>
+                            <img src='/rocket.png' alt='Upgrade' className='sidebar-icon'/>
+                            <span>Upgrade to Pro</span>
+                        </div>
                     </div>
                 </div>
                 <div className='main'>
                     <div className='chats'>
-                        <div className='chat user'>
-                            <p className='txt'>Hello!</p>
-                        </div>
-                        <div className='chat bot'>
-                            <p className='txt'>Hi there! How can I assist you today?</p>
-                        </div>
+                        {chat.map((msg, index) => (
+                            <div key={index} className={`chat ${msg.sender}`}>
+                                <p className='txt'>{msg.text}</p>
+                            </div>
+                        ))}
                     </div>
                     <div className='chat-footer'>
-                        <input type='text' placeholder='Send a Message' className='chat-input' />
-                        <button className='send-btn'><img src='/enviar.png' alt='Send' className='send-icon' /></button>
+                        <input type='text' placeholder='Send a Message' className='chat-input' value={message} onChange={(e) => setMessage(e.target.value)} />
+                        <button className='send-btn' onClick={sendMessage}><img src='/enviar.png' alt='Send' className='send-icon' /></button>
                     </div>
                 </div>
             </div>
