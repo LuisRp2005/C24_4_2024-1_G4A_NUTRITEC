7b7dc5e7cad39c56b07fe8b466795591b8f57307
alvarocc2004 <149338774+AlvaroCangalaya@users.noreply.github.com>
2024-06-21 00:19:48 -0500

    a
    a

--- nutri/nutri/settings.py
+++ nutri/nutri/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,8 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+from pathlib import Path
 
+# Definir la ruta base de tu proyecto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Configuración para archivos estáticos y multimedia
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
